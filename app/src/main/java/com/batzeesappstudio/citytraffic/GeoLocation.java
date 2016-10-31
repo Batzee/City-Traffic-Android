13623f52d41c97d95c89cdea78ed6e28dc59e8c4
Amalan Dhananjayan <amalan.dhananjayan@ifsworld.com>
2016-10-31 08:36:33 +0530

    Revert "As everyone seems finding it difficult I have added My Own Firebase key hope now it works. But you wont be able to check the DB entries. So if you want to check them you have to create your own account and add your own key."
    This reverts commit 88b1ea6a2f368f6311eed2dd4e9c9809880c0653.

--- app/src/main/java/com/batzeesappstudio/citytraffic/GeoLocation.java
+++ app/src/main/java/com/batzeesappstudio/citytraffic/GeoLocation.java
@@ -7,8 +7,8 @@ import java.util.Map;
  * Created by adh on 7/20/2016.
  */
 public class GeoLocation {
-    private String userID;
-    private String userName;
+    private String userid;
+    private String username;
     private String type;
     private String longtitude;
     private String latitude;
@@ -19,18 +19,59 @@ public class GeoLocation {
         /*Blank default constructor essential for Firebase*/
     }
 
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getDisplayName() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLongtitude(String longtitude) {
+        this.longtitude = longtitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
     public void setID(String id) {
-        this.userID = id;
+        this.userid = id;
     }
     public String getID() {
-        return userID;
+        return userid;
     }
 
     public void setName(String name) {
-        this.userName = name;
+        this.username = name;
     }
     public String getName() {
-        return userName;
+        return username;
     }
 
     public void setType(String locType) {
@@ -63,8 +104,8 @@ public class GeoLocation {
 
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
-        result.put("userid", userID);
-        result.put("username", userName);
+        result.put("userid", userid);
+        result.put("username", username);
         result.put("type", type);
         result.put("longti", longtitude);
         result.put("lati", latitude);
